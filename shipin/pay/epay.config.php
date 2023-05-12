93b301e741292d55fdcce8a97dfc1a1e19ab4b1c
patrickkaku <133326363+patrickkaku@users.noreply.github.com>
2023-05-12 14:47:14 +0800

    Add files via upload

---
+++ shipin/pay/epay.config.php
@@ -0,0 +1,13 @@
+<?php
+/* *
+ * 配置文件
+ */
+
+$alipay_config['partner']		= '1';//商户id
+$alipay_config['key']			= '886QLI8H';///商户key
+$alipay_config['sign_type']    = strtoupper('MD5');
+$alipay_config['input_charset']= strtolower('utf-8');
+$alipay_config['transport']    = 'http';
+$alipay_config['apiurl']    =  'https://www.92luntan.com/'; //网关无需改动
+//易支付免签支付（微信支付宝免挂支付）：https://www.pay.xcooo.cn/
+?>
